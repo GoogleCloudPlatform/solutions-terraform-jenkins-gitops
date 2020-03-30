5cdec62361d9fa8567e1bda539540c353f45bbf6
Rahul Gupta <guptarhl@google.com>
2020-03-29 19:08:54 -0700

    added a variable for environment to be used in naming of resources

--- example-pipelines/environments/dev/terraform.tfvars
+++ example-pipelines/environments/dev/terraform.tfvars
@@ -1,5 +1,5 @@
 project_id = "PROJECT_ID"
-vpc_network_name = "example-vpc-dev"
+environment = "dev"
 subnet1_region = "us-central1"
 subnet1_zone = "us-central1-a"
 subnet1_cidr = "100.0.0.0/17"
--- example-pipelines/example-create/main.tf
+++ example-pipelines/example-create/main.tf
@@ -15,6 +15,14 @@
  */
 
 /*****************************************
+  Locals
+ *****************************************/
+locals {
+  vpc_network_name = "example-vpc-${var.environment}"
+  vm_name = "example-vm-${var.environment}-001"
+}
+
+/*****************************************
   Google Provider Configuration
  *****************************************/
 provider "google" {
@@ -28,11 +36,11 @@ module "gcp-network" {
   source       = "terraform-google-modules/network/google"
   version      = "~> 1.4.0"
   project_id   = var.project_id
-  network_name = var.vpc_network_name
+  network_name = local.vpc_network_name
 
   subnets = [
     {
-      subnet_name   = "${var.vpc_network_name}-${var.subnet1_region}"
+      subnet_name   = "${local.vpc_network_name}-${var.subnet1_region}"
       subnet_ip     = var.subnet1_cidr
       subnet_region = var.subnet1_region
     },
@@ -45,7 +53,7 @@ module "gcp-network" {
 resource "google_compute_instance" "vm_0001" {
   project      = var.project_id
   zone         = var.subnet1_zone
-  name         = "${var.project_id}-vm-0001"
+  name         = local.vm_name
   machine_type = "f1-micro"
   network_interface {
     network    = module.gcp-network.network_name
