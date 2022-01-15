5868a5d76d9a8b982814e8145902ae3baa926cc8
liyy.inspurworld <liyy.inspurworld@gmail.com>
2022-01-15 06:25:51 +0000

    Change example vm 2

--- example-pipelines/example-create/main.tf
+++ example-pipelines/example-create/main.tf
@@ -51,10 +51,28 @@ module "gcp-network" {
   Create a GCE VM Instance
  *****************************************/
 resource "google_compute_instance" "vm_0001" {
-  count = 3
   project      = var.project_id
   zone         = var.subnet1_zone
   name         = local.vm_name
+  machine_type = "f1-micro"
+  network_interface {
+    network    = module.gcp-network.network_name
+    subnetwork = module.gcp-network.subnets_self_links[0]
+  }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+}
+
+/*****************************************
+  Create a GCE VM Instance 2
+ *****************************************/
+resource "google_compute_instance" "vm_0002" {
+  project      = var.project_id
+  zone         = var.subnet1_zone
+  name         = vm_0002
   machine_type = "f1-micro"
   network_interface {
     network    = module.gcp-network.network_name
